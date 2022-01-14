75fcdc53351a62a9066a935bfc6559399dff00fc
willyoupleasefeedme <lucasdan7276@gmail.com>
2022-01-14 23:11:18 +0000

    updated to check for open port

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-
-import os
 import secret
+import socket
 import fbchat
-from fbchat.models import *
+from fbchat.models import Message
 import time
 
 starttime = time.time()
 
 hostname = secret.hostname
-while True:
-    # -c linux, -n windows
-    response = os.system("ping -c 1 " + hostname)  # + " > /dev/null 2>&1")
+def main():
+	while True:
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+			result = sock.connect_ex((hostname, 80))
+		
+		if result == 0:
+			print("sucess")
+		else:
+			print("failure")
+			client = fbchat.Client(secret.username, secret.pw)
+			client.send(Message(text="server down :) -bot"), thread_id=secret.friendo,
+						thread_type=ThreadType.USER)
+			time.sleep(43200.0 - ((time.time() - starttime) %
+					   43200.0))  # 43200 sleep for 12h
 
-    if response == 0:
-        print("sucess")
-    else:
-        print("failure")
-        client = fbchat.Client(secret.username, secret.pw)
-        client.send(Message(text="server down :) -bot"), thread_id=secret.friendo,
-                    thread_type=ThreadType.USER)
-        time.sleep(43200.0 - ((time.time() - starttime) %
-                   43200.0))  # 43200 sleep for 12h
+		time.sleep(3600.0 - ((time.time() - starttime) % 3600.0))  # 3600 sleep 1h
 
-    time.sleep(3600.0 - ((time.time() - starttime) % 3600.0))  # 3600 sleep 1h
+if __name__ == "__main__":
+    main()
